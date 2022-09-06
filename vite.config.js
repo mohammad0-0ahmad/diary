dbad3ec2f82290e57d8f0c39d9848e61d4e89e33
Mohammad Ahmad <kurtular.vadisi@gmail.com>
2022-09-06 23:34:21 +0000

    Adding vue js as frontend framework

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
+import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
     server: {
@@ -9,8 +10,21 @@ export default defineConfig({
     },
     plugins: [
         laravel({
-            input: ["resources/css/app.css", "resources/js/app.js"],
+            input: ["resources/sass/app.scss", "resources/js/app.js"],
             refresh: true,
         }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
     ],
+    resolve: {
+        alias: {
+            vue: "vue/dist/vue.esm-bundler.js",
+        },
+    },
 });
