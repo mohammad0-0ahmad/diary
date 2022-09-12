73c58f5a64b5e6208b2575815ac52c6953eacd8e
Mohammad Ahmad <kurtular.vadisi@gmail.com>
2022-09-12 20:38:56 +0200

    Improve layout and welcome page.

---
+++ lang/en/home.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'welcome-to' => "Welcome to",
+    'intro' => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Interdum curabitur ornare posuere sed. Nunc, pharetra ut pellentesque pretium lacus. Nisl, et egestas tempus ipsum sit velit vel. Posuere quam sit sit id nulla.",
+
+];
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,12 +1,22 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-                    {{ __('You are not logged in!') }}
+    <div class="container-fluid h-100 mt-5 pe-0 mt-auto">
+        <div class="row justify-content-end align-items-center mw-100 me-0 mt-auto">
+            <div class="row col-md-6 mb-5">
+                <span class="row fs-1 fw-bold"> {{ __('home.welcome-to') }}</span>
+                <div class="col-5 col-xs-8 my-5 ">
+                    <img src="/images/diary-logo.png" class="w-100" />
+                </div>
+                <div lass="row">
+                    <h3 class="col-xs-11 col-md-10  col-lg-8"> {{ __('home.intro') }}</h3>
+                </div>
             </div>
+            <div class="row col-md-6 justify-content-end align-items-end px-0">
+                <hero></hero>
+            </div>
+
         </div>
     </div>
-</div>
+    </div>
 @endsection
